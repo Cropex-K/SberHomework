bf888f935bf585031e9d86f861887c9316aeac63
Cropex-K <berdnikova-k@mail.ru>
2021-04-28 19:42:51 +0300

    в зоопарк добавили исключения

--- Zoo/src/com/zoo/Main.java
+++ Zoo/src/com/zoo/Main.java
@@ -1,4 +1,23 @@
 package com.zoo;
 
+import com.zoo.animal.Lama;
+import com.zoo.exception.SomeException2;
+import com.zoo.exception.WrongDataException;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Lama lama1 = new Lama();
+        try {
+            lama1.giveTreat(-1);
+        } catch (WrongDataException e) {
+            System.out.println("Неправильное количество вкусняшек :)");;
+        }
+
+        try {
+            lama1.giveTreat(null);
+        } catch (SomeException2 someException2) {
+            System.out.println("Необходимо дать существующую вкусняшку");;
+        }
+    }
 }
--- Zoo/src/com/zoo/animal/Animal.java
+++ Zoo/src/com/zoo/animal/Animal.java
@@ -1,5 +1,8 @@
 package com.zoo.animal;
 
+import com.zoo.exception.SomeException2;
+import com.zoo.exception.WrongDataException;
+
 public abstract class Animal {
     private String name;
 
@@ -25,12 +28,22 @@ public abstract class Animal {
 
     public abstract void play();
 
-    public void giveTreat (int i){
-        System.out.println("Вы угостили животное "+ i+ " раз");
+    public void giveTreat (int i) throws WrongDataException {
+        if (i<=0){
+            throw new WrongDataException();
+        }
+       else{
+           System.out.println("Вы угостили животное "+ i+ " раз");
+        }
     }
 
-    public void giveTreat (String s){
-        System.out.println("Вы угостили животное вкусняшкой - "+ s);
+    public void giveTreat (String s) throws SomeException2 {
+        if (s.equals(null)){
+            throw new SomeException2();
+        }
+        else{
+            System.out.println("Вы угостили животное вкусняшкой - "+ s);
+        }
     }
 
 }
