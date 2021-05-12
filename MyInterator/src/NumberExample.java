8cd77bb59ac6ce418c132c62639445335830bbfd
Cropex-K <berdnikova-k@mail.ru>
2021-05-12 20:12:14 +0300

    Итераторы

---
+++ MyInterator/src/NumberExample.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class NumberExample {
+    public static void main(String[] args) {
+        Number number = new Number(123456);
+
+        for (Integer num : number){
+            System.out.print(num + " ");
+        }
+
+    }
+}
